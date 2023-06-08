b2dabf9132d9536f84200465504e196492aae16c
mourad <mourad.latoui.89@gmail.com>
2023-06-08 18:29:04 -0400

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+
+const colors = [];
 
 function App() {
+  useEffect(() => {
+    console.log('h2');
+    // const randomIndex = Math.floor(Math.random());
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="current-color">Test</h1>
+      <div classname="colors"></div>
     </div>
   );
 }
