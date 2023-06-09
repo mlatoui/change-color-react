796b10f5530cf1aa9b1c0bb3d8133f0a76134ca6
mourad <mourad.latoui.89@gmail.com>
2023-06-09 16:03:36 -0400

    js html to react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import './app.css';
 
-const colors = [];
+const colors = [
+  'crimson',
+  'deeppink',
+  'deepskyblue',
+  'gold',
+  'lightgrey',
+  'pink',
+  'orange',
+  'mediumturquoise',
+  'lemonchiffon',
+  'indigo',
+];
 
 function App() {
+  const [state, setState] = useState({ colorIndex: 0 });
+
   useEffect(() => {
-    console.log('h2');
-    // const randomIndex = Math.floor(Math.random());
+    setState({ colorIndex: Math.floor(Math.random() * colors.length) });
   }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = colors[state.colorIndex];
+  }, [state.colorIndex]);
+
   return (
     <div className="App">
-      <h1 id="current-color">Test</h1>
-      <div classname="colors"></div>
+      <h1 id="current-color">{colors[state.colorIndex]}</h1>
+      <div className="colors">
+        {colors.map((c, i) => {
+          return (
+            <button
+              key={`color-${i}`}
+              className={i === state.colorIndex ? 'active' : ''}
+              style={{
+                backgroundColor: c,
+              }}
+              onClick={() => {
+                setState({ colorIndex: i });
+              }}
+            ></button>
+          );
+        })}
+      </div>
     </div>
   );
 }
